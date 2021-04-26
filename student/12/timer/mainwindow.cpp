cacd450403e2cc03da333b0badfca38ed75c3c41
Huy Trinh <giahuy.trinh@tuni.fi>
2021-04-26 15:43:41 +0300

    Submit timer exercise

--- student/12/timer/mainwindow.cpp
+++ student/12/timer/mainwindow.cpp
@@ -6,9 +6,55 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    timer=new QTimer();
+    connect(timer,SIGNAL(timeout()),this,SLOT(incrementer()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::incrementer()
+{
+    int second_num = ui->lcdNumberSec->value();
+    int minute_num = ui->lcdNumberMin->value();
+    if(second_num<59) {
+        second_num++;
+    }else{
+        minute_num+=1;
+        second_num=0;
+    }
+    ui->lcdNumberSec->setPalette(Qt::red);
+    ui->lcdNumberMin->setPalette(Qt::green);
+    ui->lcdNumberSec->display(second_num);
+    ui->lcdNumberMin->display(minute_num);
+
+}
+
+void MainWindow::on_startButton_clicked()
+{
+
+    timer->start(1000);
+
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+    interval_ = timer->remainingTime();
+    if(timer->isActive()){
+        timer->stop();
+    }
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    int second_num = ui->lcdNumberSec->value();
+    int minute_num = ui->lcdNumberMin->value();
+    timer->stop();
+   second_num=0;
+   minute_num=0;
+   ui->lcdNumberMin->display(minute_num);
+    ui->lcdNumberSec->display(second_num);
 }
--- student/12/timer/mainwindow.hh
+++ student/12/timer/mainwindow.hh
@@ -23,10 +23,18 @@ public:
 
 private slots:
     // Add your slots here
+    void incrementer();
+
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_resetButton_clicked();
 
 private:
     Ui::MainWindow *ui;
     // Add other necessary members here
+    int interval_=1000;
 };
 
 #endif // MAINWINDOW_HH
