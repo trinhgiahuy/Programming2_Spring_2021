d7fde687a09d92ecdef1e169071ac6b85cb48cac
Huy Trinh <giahuy.trinh@tuni.fi>
2021-05-10 19:59:20 +0300

    Test all functions and submit

---
+++ student/13/pairs_gui/scoreboard.cpp
@@ -0,0 +1,28 @@
+#include "scoreboard.h"
+#include "QDebug"
+// this class displays player's current score
+Scoreboard::Scoreboard(QTimer* timer, QString player_name_) : QObject()
+{
+    this->player_ = new QLabel(player_name_);
+    this->label = new QLabel("Score: 0");
+    this->counter = 0;
+    this->timer = timer;
+}
+
+// A operation return the point of the scoreboard
+int Scoreboard::returnPoint()
+{
+    return counter;
+}
+
+// this slot increments the score by 1 when a queue number of scoreboard
+// match the current game turn and a pair is found
+void Scoreboard::increment(unsigned int game_turn)
+{
+    if(game_turn == queue_){
+        this->counter += 1;
+        qDebug() << "Num counter"<<counter;
+        this->label->setText("Score: " + QString::number(counter));
+        emit increasePoint(this->timer);
+    }
+}
